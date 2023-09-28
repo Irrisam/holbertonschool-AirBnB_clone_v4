b38f9e78c3f5c2d8be1a92817a3e5437fb208cf6
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-09-28 08:16:32 +0000

    add alert

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 
-$(document).ready(function() {
-    const dict = {};
-    $('input').change(function() {
-        if (this.checked) {
-            dict[$(this).attr('data-id')] = $(this).attr('data-name');
-        } else {
-            delete dict[$(this).attr('data-id')];
+$(document).ready(function () {
+    $('.checkbox').change(function () {
+        if ($('.checkbox:checked').length > 0) {
+            alert('At least one checkbox is checked!');
         }
-        $('div.amenities h4').html(Object.values(dict).join(', ') || '&nbsp;');
     });
 });
